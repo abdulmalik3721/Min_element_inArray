b53412f0c4a22a62c3954e59bca4027fc3a7a34d
ABDUL MALIK <malik.kasganj@gmail.com>
2026-05-08 16:03:27 +0530

    polymorphism

---
+++ Main4.java
@@ -0,0 +1,11 @@
+public class Main4 {
+    public static void main(String[] args) {
+        calculator calc=new calculator();
+      int ab=calc.add(89,11);
+        calc.add(98,65,978);
+        System.out.println(ab);
+
+
+    }
+    
+}
